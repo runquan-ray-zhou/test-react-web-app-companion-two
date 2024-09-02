6cfd19648330ac8016176a33cd65f034d9e1b97c
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-09-02 01:09:09 -0400

    add search bar to index page

--- src/Pages/Index/Index.jsx
+++ src/Pages/Index/Index.jsx
@@ -2,18 +2,20 @@ import { useState } from 'react'
 import { useNavigate } from "react-router-dom"
 import arLogo from "../../assets/arLogo.svg"
 import heroImage from "../../assets/heroImage.svg"
+import { september28thPresentations } from "../../assets/data"
 import "./Index.css"
 
 export default function Index() {
 
   const [showMenu, setShowMenu] = useState(false)
+  const [todaySessions, setTodaySessions] = useState(september28thPresentations)
 
   const navigate = useNavigate()
 
   const handleMenu = () => {
       setShowMenu(!showMenu)
   }
-  
+
   return (
     <div className="Index">
       <div className="Index-container">
@@ -25,18 +27,25 @@ export default function Index() {
             <li>Event Map</li>
           </ul>
         </div>
-            <nav>
-                <div className="Index-logo">
-                    <img src={heroImage} alt="index-heroImage" />
-                </div>
-                <div className={`Index-hamburger ${showMenu ? "active": ""}`} onClick={handleMenu}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
-            </nav>
-        
-
+          <nav>
+            <div className="Index-logo">
+              <img src={heroImage} alt="index-heroImage" />
+              <div className="searchbar-container">
+                <form>
+                  <input type="text" name="search" id="search" placeholder="Search" autoComplete='off'/>
+                    <button>
+                    {' '}
+                    <i className="fa-solid fa-magnifying-glass"></i>
+                    </button>
+                </form>
+              </div>
+            </div>
+            <div className={`Index-hamburger ${showMenu ? "active": ""}`} onClick={handleMenu}>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </nav>
         <div className="Index-scanner-button">
           <button onClick={() => navigate("/scanner")}>
             <img src={arLogo} alt="scanner-arLogo" />
