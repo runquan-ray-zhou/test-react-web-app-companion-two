e53e46e126ecaef2320503cb7afa5159f602350e
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-09-03 17:28:19 -0400

    complete show page for session details

--- src/Components/SessionDetails/SessionDetails.jsx
+++ src/Components/SessionDetails/SessionDetails.jsx
@@ -13,22 +13,27 @@ export default function SessionDetails() {
     <div className='SessionDetails'>
         <div className='SessionDetails-container'>
             <div className='SessionDetails-icon'>
-                <span>
+                <span className='SessionDetails-icon-fa'>
                 <i className="fa-regular fa-calendar"></i>
+                </span>
+                <span className='SessionDetails-icon-span'>
+                    <p>{session.type}</p>
                 </span>
             </div>
             <span>
-                <p className='SessionDetails-title'>{session.title}</p>
+                <h3 className='SessionDetails-title'>{session.title}</h3>
             </span>
             <span>
-                <p className='SessionDetails-time'>{session.time}</p>
+                <h3 className='SessionDetails-time'>{session.time}</h3>
             </span>
-            <span>
-                <p>{Date().slice(0,15)}</p>
-            </span>
-            <span>
-                <p className='SessionDetails-location'>{session.location}</p>
-            </span>
+            <div className='SessionDetails-date-location'>
+                <span>
+                    <h3>{Date().slice(0,15)}</h3>
+                </span>
+                <span>
+                    <p className='SessionDetails-location'>Location:{session.location}</p>
+                </span>
+            </div>
             <div className='SessionDetails-call-to-action'>
                 <hr />
                 <button>
@@ -36,37 +41,40 @@ export default function SessionDetails() {
                 </button>
                 <hr />
             </div>
-            {session.sessionLeader ? <span>
-                <p className='SessionDetails-session-leader'>Session Leader:</p>
-                <p className='SessionDetails-session-leader-name'>{session.sessionLeader.split(" – ")[0]}<br></br>{session.sessionLeader.split(" – ")[1]}</p>
-            </span> : null}
-            <span>
-            <p className="SessionDetails-presenters">Session Presenters: <br></br>{session.presenters ? session.presenters.map(presenter => <p className="Session-info-presenter">{presenter.split(" – ")[0]}<br></br>{presenter.split(" – ")[1]}</p>) : null}</p>
-            </span>
-            <span>
-                <p className='SessionDetails-description'>
-                    {!Array.isArray(session.description) 
-                        ? session.description 
-                        : session.description.map((item, index) => (
-                            <div key={index} className="presentation-item">
-                            <h3>{item.title}</h3>
-                            {item.description && <p>{item.description}</p>}
-                            <div className="presenters">
-                                <h4>Presenters:</h4>
-                                <ul>
-                                {item.presenters.map((presenter, presenterIndex) => (
-                                    <li key={presenterIndex}>
-                                    <strong>{presenter.name}</strong>
-                                    {presenter.affiliation && ` - ${presenter.affiliation}`}
-                                    </li>
-                                ))}
-                                </ul>
-                            </div>
-                            </div>
-                        ))
-                    }
-                    </p>
-            </span>
+            <div className='SessionDetails-session-info'>
+                {session.sessionLeader ? <span>
+                    <p className='SessionDetails-session-leader'>Session Leader:</p>
+                    <p className='SessionDetails-session-leader-name'><h4>{session.sessionLeader.split(" – ")[0]}</h4>{session.sessionLeader.split(" – ")[1]}</p>
+                </span> : null}
+                {session.presenters ? <span>
+                    <p className='SessionDetails-presenters'>Session Presenters:</p>
+                    <p className='SessionDetails-presenters-names'>{session.presenters.map(presenter => <p className='SessionDetails-presenters-names-names'><h4>{presenter.split(" – ")[0]}</h4>{presenter.split(" – ")[1]}</p>)}</p>
+                </span> : null}
+                <span>
+                    <p className='SessionDetails-description'>
+                        {!Array.isArray(session.description) 
+                            ? session.description 
+                            : session.description.map((item, index) => (
+                                <div key={index} className="presentation-item">
+                                <h3>{item.title}</h3>
+                                {item.description && <p>{item.description}</p>}
+                                <div className="presenters">
+                                    <h3>Presenters:</h3>
+                                    <ul>
+                                    {item.presenters.map((presenter, presenterIndex) => (
+                                        <li key={presenterIndex}>
+                                        <strong>{presenter.name}</strong>
+                                        {presenter.affiliation && ` - ${presenter.affiliation}`}
+                                        </li>
+                                    ))}
+                                    </ul>
+                                </div>
+                                </div>
+                            ))
+                        }
+                        </p>
+                </span>
+            </div>
         </div>
     </div>
   )
