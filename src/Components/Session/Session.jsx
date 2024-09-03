84838c37212bf7f2b0d0b4d58b9ebd4d023940a3
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-09-03 01:22:33 -0400

    add search functionality to search bar and complete session list page

--- src/Components/Session/Session.jsx
+++ src/Components/Session/Session.jsx
@@ -1,9 +1,23 @@
+import "./Session.css"
+
 export default function Session({session}) {
     return (
-    <div className="Session">
-        <p>{session.time}</p>
-        <p>{session.title}</p>
-        <p>{session.location}</p>
+      <div className="Session">
+          <div className="Session-icon">
+            <span>
+              <i className="fa-regular fa-calendar"></i>
+            </span>
+          </div>
+          <div className="Session-info">
+            <p className="Session-info-title">{session.title}</p>
+            <p className="Session-info-time">{session.time}</p>
+            <p className="Session-info-leader">Session Leader: {session.sessionLeader ? session.sessionLeader.split(" – ")[0] : null}</p>
+            <p className="Session-info-presenters">Session Presenters: <br></br>{session.presenters ? session.presenters.map(presenter => <p className="Session-info-presenter">{presenter.split(" – ")[0]}</p>) : null}</p>
+            <p className="Session-info-location">Location: {session.location}</p>
+          </div>
+        <div className="Session-arrow">
+          <i className="fa-solid fa-chevron-right"></i>
+        </div>
       </div>
     )
 }
--- src/Pages/Index/Index.jsx
+++ src/Pages/Index/Index.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useNavigate } from "react-router-dom"
 import arLogo from "../../assets/arLogo.svg"
 import heroImage from "../../assets/heroImage.svg"
@@ -10,11 +10,39 @@ export default function Index() {
 
   const [showMenu, setShowMenu] = useState(false)
   const [todaySessions, setTodaySessions] = useState(september28thPresentations)
+  const [filteredSessions, setFilteredSessions] = useState([])
+  const [searchQuery, setSearchQuery] = useState('');
 
   const navigate = useNavigate()
 
   const handleMenu = () => {
       setShowMenu(!showMenu)
+  }
+
+  useEffect(() => {
+    setTodaySessions(september28thPresentations)
+  }, [])
+
+  useEffect(() => {
+    setFilteredSessions([...todaySessions])
+  }, [todaySessions.length]) 
+
+  const handleTextChange = (e) => {
+    const searchTerm = e.target.value.toLowerCase()
+    setSearchQuery(searchTerm)
+    const filteredSessions = searchTerm.length ? filterSessions(searchTerm, todaySessions) : todaySessions;
+    setFilteredSessions(filteredSessions)
+  }
+
+  function filterSessions(searchTerm, allSessions) {
+    return allSessions.filter((session) => {
+      if (!session.presenters || !Array.isArray(session.presenters)) {
+        return false;
+      }
+      return session.presenters.some(presenter => 
+        presenter.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    });
   }
 
   return (
@@ -33,7 +61,14 @@ export default function Index() {
               <img src={heroImage} alt="index-heroImage" />
               <div className="searchbar-container">
                 <form>
-                  <input type="text" name="search" id="search" placeholder="Search" autoComplete='off'/>
+                  <input 
+                  type="text" 
+                  name="search" 
+                  id="search" 
+                  placeholder="Search Presenter" 
+                  autoComplete='off'
+                  onChange={handleTextChange}
+                  />
                     <button>
                     {' '}
                     <i className="fa-solid fa-magnifying-glass"></i>
@@ -48,7 +83,7 @@ export default function Index() {
             </div>
           </nav>
         <div className="Index-sessions">
-          <Sessions todaySessions={todaySessions}/>
+          <Sessions todaySessions={filteredSessions}/>
         </div>
         <div className="Index-scanner-button">
           <button onClick={() => navigate("/scanner")}>
