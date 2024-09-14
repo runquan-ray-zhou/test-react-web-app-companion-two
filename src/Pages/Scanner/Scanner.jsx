ee765e95f930953e565488ad9b0acbb110f09d6f
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-09-14 02:21:46 -0400

    change scanner page

--- src/Pages/Scanner/Scanner.jsx
+++ src/Pages/Scanner/Scanner.jsx
@@ -1,31 +1,30 @@
-import { Link, useNavigate } from "react-router-dom"
-import arLogo from "../../assets/arLogo.svg"
-import "./Scanner.css"
+import NavBar from "../../Components/NavBar/NavBar";
+import "./Scanner.css";
 
 export default function Scanner() {
-    const navigate = useNavigate()
   return (
     <div className="Scanner">
-        <div className="Scanner-container">
-            <nav className="Scanner-container-nav">
-                    <i onClick={() => navigate("/home")} className="fa-solid fa-chevron-left"></i><h1>Scanner</h1>
-            </nav>
-            <div className="Scanner-iframe">
-                <iframe
-                    style={{border: "none"}}
-                    src="https://test-ar-marker-react.netlify.app/"
-                    allow="camera"
-                ></iframe>
-            </div>
-            <div className="Scanner-message">
-                <p>Point your camera at a OR code to start scanning and discover more event details!</p>
-            </div>
-            <div className="Scanner-button">
-                <button onClick={() => navigate("/scanner")}>
-                    <img src={arLogo} alt="scanner-arLogo" />
-                </button>
-            </div>
+      <div className="Scanner-container">
+        <NavBar />
+        <div className="Scanner-iframe">
+          <iframe
+            style={{ border: "none" }}
+            src="https://test-ar-marker-react.netlify.app/"
+            allow="camera"
+          ></iframe>
         </div>
+        <div className="Scanner-message">
+          <p>
+            Point your camera at a QR code, then click the 3D image that appears
+            to view location information!
+          </p>
+        </div>
+        {/* <div className="Scanner-button">
+          <button onClick={() => navigate("/scanner")}>
+            <img src={arLogo} alt="scanner-arLogo" />
+          </button>
+        </div> */}
+      </div>
     </div>
-  )
+  );
 }
