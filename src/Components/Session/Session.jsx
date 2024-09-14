89f3ff892e07a9739cb8bdeb0e5a911acdf3a05a
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-09-13 22:05:24 -0400

    update home/landing page

--- src/Components/Session/Session.jsx
+++ src/Components/Session/Session.jsx
@@ -1,26 +1,40 @@
 import { Link } from "react-router-dom";
-import "./Session.css"
+import "./Session.css";
 
-export default function Session({session}) {
-    return (
-      <Link to={`/sessions/${session.id}`} className="Session-link">
+export default function Session({ session }) {
+  return (
+    <Link to={`/sessions/${session.id}`} className="Session-link">
       <div className="Session">
-          <div className="Session-icon">
-            <span>
-              <i className="fa-regular fa-calendar"></i>
-            </span>
-          </div>
-          <div className="Session-info">
-            <p className="Session-info-title">{session.title}</p>
-            <p className="Session-info-time">{session.time}</p>
-            <p className="Session-info-leader">Session Leader: {session.sessionLeader ? session.sessionLeader.split(" – ")[0] : null}</p>
-            <p className="Session-info-presenters">Session Presenters: <br></br>{session.presenters ? session.presenters.map(presenter => <p className="Session-info-presenter">{presenter.split(" – ")[0]}</p>) : null}</p>
-            <p className="Session-info-location">Location: {session.location}</p>
-          </div>
+        <div className="Session-icon">
+          <span>
+            <i className="fa-regular fa-calendar"></i>
+          </span>
+        </div>
+        <div className="Session-info">
+          <p className="Session-info-title">{session.title}</p>
+          <p className="Session-info-time">{session.time}</p>
+          <p className="Session-info-leader">
+            Session Leader:{" "}
+            {session.sessionLeader
+              ? session.sessionLeader.split(" – ")[0]
+              : null}
+          </p>
+          <p className="Session-info-presenters">
+            Session Presenters: <br></br>
+            {session.presenters
+              ? session.presenters.map((presenter) => (
+                  <p className="Session-info-presenter">
+                    {presenter.split(" – ")[0]}
+                  </p>
+                ))
+              : null}
+          </p>
+          <p className="Session-info-location">Location: {session.location}</p>
+        </div>
         <div className="Session-arrow">
           <i className="fa-solid fa-chevron-right"></i>
         </div>
       </div>
-      </Link>
-    )
+    </Link>
+  );
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,53 +1,69 @@
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
-import arLogo from "../../assets/arLogo.svg"
-import heroImage from "../../assets/heroImage.svg"
-import Carousel from "../../Components/Carousel/Carousel"
-import "./Home.css"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import arLogo from "../../assets/arLogo.svg";
+import Carousel from "../../Components/Carousel/Carousel";
+import navHero from "../../assets/navHero.svg";
+import "./Home.css";
 
 export default function Home() {
+  const [showMenu, setShowMenu] = useState(false);
 
-    const [showMenu, setShowMenu] = useState(false)
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
-
-    const handleMenu = () => {
-        setShowMenu(!showMenu)
-    }
+  const handleMenu = () => {
+    setShowMenu(!showMenu);
+  };
 
   return (
     <div className="Home">
       <div className="Home-container">
-            <div className={`off-screen-menu ${showMenu ? "active" : ""}`}>
-                <ul>
-                    <li onClick={() => navigate("/home")}>Home</li>
-                    <li onClick={() => navigate("/sessions")}>Full Session List</li>
-                    <li onClick={() => navigate("/scanner")}>Information Scan</li>
-                    <li>Event Map</li>
-                </ul>
+        <div className={`off-screen-menu ${showMenu ? "active" : ""}`}>
+          <ul>
+            <li onClick={() => navigate("/")}>Home</li>
+            <li onClick={() => navigate("/sessions")}>Full Session List</li>
+            <li onClick={() => navigate("/scanner")}>Information Scan</li>
+            <li>Event Map</li>
+          </ul>
+        </div>
+        <nav>
+          <div className="Home-logo">
+            <img src={navHero} alt="home-navHero" />
+          </div>
+          <div
+            className={`Home-hamburger ${showMenu ? "active" : ""}`}
+            onClick={handleMenu}
+          >
+            <span></span>
+            <span></span>
+          </div>
+        </nav>
+        <div className="Home-container">
+          <div className="Home-info">
+            <div className="Home-title">
+              <h2>Welcome To ASTC Chicago 2024!</h2>
             </div>
-            <nav>
-                <div className="Home-logo">
-                    <img src={heroImage} alt="home-heroImage" />
-                </div>
-                <div className={`Home-hamburger ${showMenu ? "active": ""}`} onClick={handleMenu}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
-            </nav>
+            <div className="Home-featured-title">
+              <h3>Explore Featured Events</h3>
+            </div>
             <div className="Home-date">
-                <h2>{Date().slice(0,15)}</h2>
+              <div className="Session-icon">
+                <span>
+                  <i className="fa-regular fa-calendar"></i>
+                </span>
+              </div>
+              <h4>{Date().slice(0, 15)}</h4>
             </div>
-            <div className="Home-carousel">
-                <Carousel />
-            </div>
-            <div className="Home-scanner-button">
-                <button onClick={() => navigate("/scanner")}>
-                    <img src={arLogo} alt="scanner-arLogo" />
-                </button>
-            </div>
+          </div>
+          <div className="Home-carousel">
+            <Carousel />
+          </div>
+          <div className="Home-scanner-button">
+            <button onClick={() => navigate("/scanner")}>
+              <img src={arLogo} alt="scanner-arLogo" />
+            </button>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
