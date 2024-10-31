873de8c6c67b4b495499d33c88644bf7eec3e09b
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-10-31 19:57:44 -0400

    update modal on scanner page to stay indefinite until user clicks off of it

--- src/Pages/Scanner/Scanner.jsx
+++ src/Pages/Scanner/Scanner.jsx
@@ -1,87 +1,92 @@
-import { ARCanvas, ARMarker } from '@artcom/react-three-arjs/lib/ar';
-import NavBar from '../../Components/NavBar/NavBar';
-import './Scanner.css';
-import { memo, useEffect, useState } from 'react';
-import Overlay from '../../Components/overlay/overlay';
-import Modal from '../../Components/Modal/Modal';
+import { ARCanvas, ARMarker } from "@artcom/react-three-arjs/lib/ar";
+import NavBar from "../../Components/NavBar/NavBar";
+import "./Scanner.css";
+import { memo, useEffect, useState } from "react";
+// import Overlay from '../../Components/overlay/overlay';
+import Modal from "../../Components/Modal/Modal";
 
 const data = [
-	{
-		title: 'How to Build a React App',
-		time: '9:00 AM - 10:00 AM',
-		presenter: 'John Doe',
-		location: 'Room 101',
-	},
-	{
-		title: 'How to Build a React App',
-		time: '9:00 AM - 10:00 AM',
-		presenter: 'John Doe',
-		location: 'Room 101',
-	},
+  {
+    title: "How to Build a React App",
+    time: "9:00 AM - 10:00 AM",
+    presenter: "John Doe",
+    location: "Room 101",
+  },
+  {
+    title: "How to Build a React App",
+    time: "9:00 AM - 10:00 AM",
+    presenter: "John Doe",
+    location: "Room 101",
+  },
 ];
 
 export default function Scanner() {
-	const [overlayVisible, setOverlayVisible] = useState(false);
-	const [markerFound, setMarkerFound] = useState(false);
+  const [overlayVisible, setOverlayVisible] = useState(false);
+  const [markerFound, setMarkerFound] = useState(false);
 
-	useEffect(() => {
-		const timer = setTimeout(() => {
-			setOverlayVisible(markerFound);
-		}, 300);
+  const handleBackgroundClick = (e) => {
+    if (e.target.classList.contains("overlay")) {
+      setOverlayVisible(false);
+    }
+  };
 
-		return () => {
-			clearTimeout(timer);
-		};
-	}, [markerFound]);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setOverlayVisible(markerFound);
+    }, 300);
 
-	return (
-		<div className='Scanner-container'>
-			<NavBar />
-			<Memars setMarkerFound={setMarkerFound} />
-			{overlayVisible && (
-				<div className='overlay'>
-					<Modal />
-				</div>
-			)}
-			{!overlayVisible && (
-				<div className='Scanner-message'>
-					<p>Point your camera at a Marker to begin.</p>
-				</div>
-			)}
-		</div>
-	);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [markerFound]);
+
+  return (
+    <div className="Scanner-container">
+      <NavBar />
+      <Memars setMarkerFound={setMarkerFound} />
+      {overlayVisible && (
+        <div className="overlay" onClick={handleBackgroundClick}>
+          <Modal />
+        </div>
+      )}
+      {!overlayVisible && (
+        <div className="Scanner-message">
+          <p>Point your camera at a Marker to begin.</p>
+        </div>
+      )}
+    </div>
+  );
 }
-
 function ARS({ setMarkerFound }) {
-	return (
-		<ARCanvas
-			camera={{ position: [0, 0, 0] }}
-			onCreated={({ gl }) => {
-				gl.setSize(window.innerWidth, window.innerHeight);
-			}}
-			onCameraStreamReady={() => console.log('Camera stream ready')}
-			onCameraStreamError={() => console.error('Camera stream error')}
-			sourceType={'webcam'}
-		>
-			{/* <ambientLight /> */}
-			{/* <pointLight position={[10, 10, 0]} intensity={10.0} /> */}
-			<ARMarker
-				debug={true}
-				params={{ smooth: true }}
-				type={'pattern'}
-				preset='custom'
-				patternUrl={'data/marker5.patt'}
-				onMarkerFound={() => {
-					console.log('Marker Found');
-					setMarkerFound(true);
-				}}
-				onMarkerLost={() => {
-					console.log('Marker Lost');
-					setMarkerFound(false);
-				}}
-			></ARMarker>
-		</ARCanvas>
-	);
+  return (
+    <ARCanvas
+      camera={{ position: [0, 0, 0] }}
+      onCreated={({ gl }) => {
+        gl.setSize(window.innerWidth, window.innerHeight);
+      }}
+      onCameraStreamReady={() => console.log("Camera stream ready")}
+      onCameraStreamError={() => console.error("Camera stream error")}
+      sourceType={"webcam"}
+    >
+      {/* <ambientLight /> */}
+      {/* <pointLight position={[10, 10, 0]} intensity={10.0} /> */}
+      <ARMarker
+        debug={true}
+        params={{ smooth: true }}
+        type={"pattern"}
+        preset="custom"
+        patternUrl={"data/marker5.patt"}
+        onMarkerFound={() => {
+          console.log("Marker Found");
+          setMarkerFound(true);
+        }}
+        onMarkerLost={() => {
+          console.log("Marker Lost");
+          setMarkerFound(false);
+        }}
+      ></ARMarker>
+    </ARCanvas>
+  );
 }
 
 const Memars = memo(ARS);
